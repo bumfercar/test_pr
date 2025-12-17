8526c3dc97cd2ebfc5f89c160860af72a2ad8d7e
bumfercar <choiprpiqy@naver.com>
2025-12-17 14:33:09 +0900

    수정

--- src/app.js
+++ src/app.js
@@ -1,13 +1,9 @@
+// src/app.js
 
-function multiplyd(a, b) {
-    return a * b;
-}
+const AWS_SECRET_KEY = "AKIA1234567890ABCDE"; // ❌ 보안 위반
+
 function add(a, b) {
     return a + b;
 }
-function subtract(a, b) {
-    return a - b;
-}
-
 
 console.log(add(2, 3));
